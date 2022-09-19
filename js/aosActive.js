ac3aacad4ca25f2f38656fdb0272e5bd8448c34d
YouFive <84190472+publisher-V@users.noreply.github.com>
2022-09-20 02:14:53 +0900

    max-width

--- js/aosActive.js
+++ js/aosActive.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 function AOS_MOBILE() {
-  if (matchMedia("screen and (max-width: 1280px)").matches) {
+  if (matchMedia("screen and (max-width: 1299px)").matches) {
     $(
       ".index-box-content, .aboutme-right, .aboutme-left, .common-skills-image, .mysite-content, .contact-content-left, .contact-content-right"
     ).attr("data-aos", "fade-up");
