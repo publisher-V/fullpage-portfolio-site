33d987c8cb3e65402aded09a844886df28335658
YouFive <84190472+publisher-V@users.noreply.github.com>
2022-09-21 23:59:26 +0900

    html overflow

--- js/headerAction.js
+++ js/headerAction.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const html = document.querySelector("html");
 const headerElm = document.querySelector("header");
 const drawerOpen = document.querySelector(".drawer-open");
 const drawerClose = document.querySelector(".drawer-close");
@@ -10,18 +11,21 @@ function headerOpen() {
   gnb.classList.add("show");
   progress.classList.add("hide");
   body.style.overflow = "hidden";
+  html.style.overflow = "hidden";
 }
 
 function headerClose() {
   gnb.classList.remove("show");
   progress.classList.remove("hide");
   body.style.overflowY = "auto";
+  html.style.overflow = "auto";
 }
 
 function menuClickClose() {
   gnb.classList.remove("show");
   progress.classList.remove("hide");
   body.style.overflowY = "auto";
+  html.style.overflow = "auto";
 }
 
 for (let i = 0; i < menuClick.length; i++) {
--- js/loadingPage.js
+++ js/loadingPage.js
@@ -1,10 +1,12 @@
 const loadingPage = document.querySelector(".page-loading");
 const bodyScrollY = document.querySelector("body");
+const htmlScrollY = document.querySelector("html");
 
 function loadingPageOut() {
   setTimeout(function () {
     loadingPage.classList.add("hide");
     bodyScrollY.classList.add("scroll");
+    htmlScrollY.classList.add("scroll");
   }, 3000);
 }
 
